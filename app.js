a479917e2d6b4b0bb4bf9d54cc61f595d6019e1b
anniehuynh <anniehuynh253@gmail.com>
2021-11-16 10:36:25 +0200

    passport done

--- app.js
+++ app.js
@@ -1,69 +1,84 @@
-'use strict';
-const express = require('express');
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
+"use strict";
+const express = require("express");
+var cookieParser = require("cookie-parser");
+const passport = require("./utils/pass");
+var session = require("express-session");
+const { json } = require("express");
 const app = express();
 const port = 3000;
+const loggedIn = (req, res, next) => {
+  //middleware function (req,res,next)
+  if (req.user) {
+    next();
+  } else {
+    res.redirect("/form");
+  }
+};
 
-app.set('views', './views');
-app.set('view engine', 'pug');
-app.use(cookieParser());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true })); //get req body to json
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true
-}));
 
-const username = 'foo';
-const password = 'bar';
+app.set("views", "./views");
+app.set("view engine", "pug");
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    cookie: {maxAge: 60000},
+    saveUninitialized: true,
+  })
+);
 
-app.get('/', (req, res) => {
-  console.log('cookies', req.cookies);
-  console.log('session', req.session);
-  res.render('home');
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+app.get("/", (req, res) => {
+  console.log("cookies", req.cookies);
+  console.log("session", req.session);
+  res.render("home");
 });
 
-app.get('/form', (req, res) => {
-  res.render('form');
+app.get("/form", (req, res) => {
+  res.render("form");
 });
 
-app.post('/login', (req, res) => {
-  if (req.body.username === username && req.body.password === password) {
-    req.session.logged = true;
-    res.redirect('/secret');
-  } else {
-    res.session.logged = false;
-    res.redirect('/form');
+app.post(
+  "/login",
+  passport.authenticate("local", { failureRedirect: "/form" }),
+  (req, res) => {
+    console.log("success");
+    res.redirect("/secret");
   }
+);
+
+app.get("/secret", loggedIn, (req, res) => {
+  res.render("secret");
 });
 
-app.get('/secret', (req, res) => {
-  if (req.session.logged === true) {
-    res.render('secret');
-  } else {
-    res.redirect('form');
-  }
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
 });
 
-app.get('/logout', (req, res) => {
-  req.session.logged = false;
-  res.send('Home <br><a href = "form">login</a>');
+app.get("/setCookie/:clr", (req, res) => {
+  res.cookie("color", req.params.clr, {
+    maxAge: 60 * 60 * 24 * 5,
+    httpOnly: true,
+  });
+  res.send("cookie set");
 });
 
-app.get('/setCookie/:clr', (req, res) => {
-  res.cookie('color', req.params.clr, { maxAge: 60 * 60 * 24 * 5, httpOnly: true });
-  res.send('cookie set');
-});
-
-app.get('/getCookie', (req, res) => {
-  console.log('cookies', req.cookies);
+app.get("/getCookie", (req, res) => {
+  console.log("cookies", req.cookies);
   res.send(`color is ${req.cookies.color}`);
 });
 
-app.get('/deleteCookie', (req, res) => {
-  res.clearCookie('color');
-  console.log('cookies', req.cookies);
+app.get("/deleteCookie", (req, res) => {
+  res.clearCookie("color");
+  console.log("cookies", req.cookies);
   res.send(`color deleted: ${req.cookies.color}`);
 });
 
